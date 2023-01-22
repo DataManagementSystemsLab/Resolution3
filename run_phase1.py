20667cbc4ff16d8e10af25d66cf3304ce40ff792
User <khalefaow@gmail.com>
2023-01-22 11:52:28 -0500

    V 0.01

--- run_phase1.py
+++ run_phase1.py
@@ -7,8 +7,7 @@ txts=[]
 
 
 folderid='0B0Rlpx3MRZ1SLVFXRFctd0t1cDA'
-#g.q=SimpleQueue()
-#g.q.put(folderid)
+download=False
 
 gauth = g.GoogleAuth()
 gauth.LocalWebserverAuth() # client_secrets.json need to be in the same directory as the script
@@ -18,26 +17,27 @@ c.failed=[]
 files=g.bfs(drive,folderid)
 
 txts=c.get_content_files(files)
-#get_contents(txts)
-Copy code
-import os
 
 
-if not os.path.exists("tmp"):
-    os.makedirs("tmp")
+if download:
 
-for s in range(0, len(txts),100):
-    g.download(files,txts,s,100)
+    if not os.path.exists("tmp"):
+        os.makedirs("tmp")
+
+    for s in range(0, len(txts),100):
+        g.download(files,txts,s,100)
+    f_file = open("failed.json", "w")
+    json.dump(c.failed, f_file)
+    f_file.close()
+    if not os.path.exists("files"):
+        os.makedirs("files")
+    c.flatten_dir("tmp","files") 
+    
 
 a_file = open("files.json", "w")
 json.dump(files, a_file)
 a_file.close()
 
-f_file = open("failed.json", "w")
-json.dump(c.failed, f_file)
-f_file.close()
 
-if not os.path.exists("files"):
-    os.makedirs("files")
-c.flatten_dir("tmp","files") 
+
 
